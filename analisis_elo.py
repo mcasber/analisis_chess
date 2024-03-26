0c875d60cef3af9d70968178f029f9373cd4552b
mcasber <145811813+mcasber@users.noreply.github.com>
2024-03-25 22:27:34 -0300

    adicione base chess2 y join.py

--- analisis_elo.py
+++ analisis_elo.py
@@ -12,6 +12,7 @@ print(df.head())
 year=2024
 df['Edad']=df['nacido'].apply(lambda x : year - x)
 
+
 df=df.fillna('euro') #Sustituir los valores nulos por una cadena df.fillna('Missing')
 
 #Graficas 
@@ -19,7 +20,7 @@ df=df.fillna('euro') #Sustituir los valores nulos por una cadena df.fillna('Miss
 #sns.set(style='whitegrid', palette='dark', font="Verdana", font_scale=0.75)
 #sns.countplot(data= df, y = 'pais') #sumar este
 #sns.catplot(data= df, y = 'pais') # o este
-
+'''
 sns.jointplot(
     data= df, 
     x = 'elo',
@@ -30,3 +31,4 @@ plt.show()
 
 print(df)
 print(df.groupby('continente').count())
+'''
---
+++ joins.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np 
+
+df1 = pd.read_csv('C:\\Users\\Mariano\\Desktop\\Chess\\chess.csv',sep=",",header=0,
+                 names = ['ranking', 'nombre', 'elo', 'norma', 'pais', 'juegos', 'nacido','continente'])
+
+df2 = pd.read_csv('C:\\Users\\Mariano\\Desktop\\Chess\\chess2.csv',sep=",",header=0,
+                 names = ['ranking', 'nombre', 'elo', 'norma', 'pais', 'juegos', 'nacido','continente','ingresos'])
+
+df3 = df1.merge(df2,left_on = 'nombre', right_on='nombre')
+
+print(df3) 
+
