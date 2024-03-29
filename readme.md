453e7913d97a1d4b5e1f5c82f77a8307b3e217f3
mcasber <145811813+mcasber@users.noreply.github.com>
2024-03-29 18:18:19 -0300

    se sumo el archivo readme

---
+++ readme.md
@@ -0,0 +1,4 @@
+El archivo 'analisis_elo' genera un DataFrame en base al archivo 'chess.csv' que tiene los datos de los 200 jugadores top de ajedrez según el indicador elo fide.
+Adiciona con el metodo apply una columna que obtiene la edad de cada jugador y grafica la data.
+El archivo 'practica' genra una grafica para ver la relacion entre la edad y el elo.
+
--- uniones.py
+++ uniones.py
@@ -5,9 +5,9 @@ df1 = pd.read_csv('C:\\Users\\Mariano\\Desktop\\Chess\\chess.csv',sep=",",header
                  names = ['ranking', 'nombre', 'elo', 'norma', 'pais', 'juegos', 'nacido','continente'])
 
 df2 = pd.read_csv('C:\\Users\\Mariano\\Desktop\\Chess\\chess2.csv',sep=",",header=0,
-                 names = ['ranking', 'nombre', 'elo', 'norma', 'pais', 'juegos', 'nacido','continente','ingresos'])
+                 names = ['ranking', 'jugador', 'elo', 'norma', 'pais', 'juegos', 'nacido','continente','ingresos'])
 
-df3 = df1.merge(df2,left_on = 'nombre', right_on='nombre')
+df3 = df1.merge(df2,left_on = 'nombre', right_on='jugador') #, how='left')
 
-print(df3) 
-
+print(df3.info()) 
+print(df1.describe())
